a9499422df30f11528e6247909ce86c99c742e80
Praneeth Suryadevara <spraneeth42@gmail.com>
2022-10-11 22:35:58 -0400

    i took the wockkkk to poland

--- Lab 07b - More Built-In Math Functions/script.js
+++ Lab 07b - More Built-In Math Functions/script.js
@@ -7,33 +7,30 @@ const readline = require('readline-sync');
 
   Author:      Praneeth Suryadevara
   Created:     06-Oct-2022
-  Updated:     06-Oct-2022
+  Updated:     11-Oct-2022
 */
 
 require('./index.js');
 
 // TASK 1 CODE STARTS HERE -->
 let amnt = parseInt(prompt("How many numbers are you entering?"));
-let numbers = [];
-let sum = 0;
 
-// Inputs
-for (let i = 0; i < amnt; i++) {
-  numbers.push(parseInt(prompt("")));
+// Set min and max as first numbers, compare the rest to them, also set sum as amx
+let max = parseInt(prompt(""));
+let min = max;
+let sum = max;
+
+// Change min and max accordingly
+for (let i = 0; i < amnt - 1; i++) {
+  let num = parseInt(prompt(""));
+  sum += num
+  max = Math.max(max, num);
+  min = Math.min(min, num);
 }
 
-// Calculations
-let max = numbers[0]; let min = numbers[0];
-for (let i = 0; i < amnt; i++) {
-  sum += numbers[i]; 
-  max = Math.max(max, numbers[i])
-  min = Math.min(min, numbers[i])
-}
-
-// Outputs
-console.log("Largest : " + max)
-console.log("Smallest : " + min)
-console.log("Rounded average: " + Math.round(sum/amnt))
+console.log("Largest : " + max);
+console.log("Smallest : " + min);
+console.log("Rounded average: " + Math.round(sum/amnt));
 // TASK 1 CODE ENDS HERE -->
 
 
@@ -42,5 +39,5 @@ let a = parseInt(prompt("a ="));
 let b = parseInt(prompt("b ="));
 let c = parseInt(prompt("c ="));
 
-console.log("x1 = " + (-b + Math.sqrt(Math.abs(b*b - 4*a*c)))/(2*a))
-console.log("x2 = " + (-b - Math.sqrt(Math.abs(b*b - 4*a*c)))/(2*a))
+console.log("x1 = " + (-b + Math.sqrt(Math.abs(b*b - 4*a*c)))/(2*a));
+console.log("x2 = " + (-b - Math.sqrt(Math.abs(b*b - 4*a*c)))/(2*a));
