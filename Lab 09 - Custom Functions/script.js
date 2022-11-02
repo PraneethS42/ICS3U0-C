23cf65238b4fd663e0314c48748d73da13aff1e6
Praneeth Suryadevara <spraneeth42@gmail.com>
2022-11-02 18:42:01 -0400

    Lab 09 Fixes

--- Lab 09 - Custom Functions/script.js
+++ Lab 09 - Custom Functions/script.js
@@ -22,9 +22,7 @@ require('./lesson.js');
 
 function milesToKm(distance) {
   if (distance < 0) {return undefined;}
-  else {return Math.round(distance*1.60934*100)/100;}
-}
+  else {return Math.round(distance*1.60934*100)/100;}}
 function saleCalculator(price, discount) {
   if (discount >= 100 || discount < 0 || price < 0) {return undefined;}
-  else {return Math.round(price*(100-discount)/100*100)/100;}
-}
+  else {return Math.round(price*(100-discount)/100*100)/100;}}
